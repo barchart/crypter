affce82df4f625f8c8342579ede1e647fce0aa16
jjongsma <jeremy@barchart.com>
2014-10-15 17:42:23 -0500

    Updated manifest

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(name='crypter',
 	can be run on a container host, and trusted Docker containers can mount the socket
 	directory to decrypt sensitive configuration values (i.e. credentials) without
 	requiring direct access to the private key.
+
+	Due to the messy state of Python encryption libraries related to S/MIME and PKCS#7,
+	OpenSSL binaries are required.
 	""",
 	author='Jeremy Jongsma',
 	author_email='jeremy@barchart.com',
@@ -16,5 +19,4 @@ setup(name='crypter',
 	scripts=['bin/crypter', 'bin/crypter-client'],
 	data_files=[
 		('/etc/crypter', ['etc/crypter.cfg'])
-	],
-	install_requires=['pycrypto>=2.6.1'])
+	])
